bff7330bb6b515f510c3033a383e2dafb23dd797
Brodingo <brodyk@gmail.com>
2025-04-20 21:21:06 -0400

    explore command

--- repl.go
+++ repl.go
@@ -12,7 +12,7 @@ import (
 type cliCommand struct {
 	name        string
 	description string
-	callback    func(*config) error
+	callback    func(*config, ...string) error
 }
 
 func getCommands() map[string]cliCommand {
@@ -36,6 +36,11 @@ func getCommands() map[string]cliCommand {
 			name:        "mapb",
 			description: "Get the previous page of locations",
 			callback:    commandMapb,
+		},
+		"explore": {
+			name:        "explore <location_name>",
+			description: "Explore a location",
+			callback:    commandExplore,
 		},
 	}
 }
@@ -64,10 +69,14 @@ func startRepl(cfg *config) {
 		// Store the first word of the input
 		commandName := words[0]
 		// args := words[1:]
+		args := []string{}
+		if len(words) > 1 {
+			args = words[1:]
+		}
 
 		command, exists := getCommands()[commandName]
 		if exists {
-			err := command.callback(cfg)
+			err := command.callback(cfg, args...)
 			if err != nil {
 				fmt.Println(err)
 			}
